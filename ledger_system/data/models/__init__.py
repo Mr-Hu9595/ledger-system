fa25995ecbbb3f186373063d683f57b676546c87
Ledger Admin <admin@ledger.local>
2026-04-30 17:58:04 +0800

    feat: export new MaterialProperty and EquipmentProperty models

--- ledger_system/data/models/__init__.py
+++ ledger_system/data/models/__init__.py
@@ -3,12 +3,24 @@ from ledger_system.data.models.base import Base
 from ledger_system.data.models.ledger import Ledger
 from ledger_system.data.models.inbound import Inbound
 from ledger_system.data.models.outbound import Outbound
+from ledger_system.data.models.material_property import MaterialProperty, MATERIAL_PROPERTY_KEYS
+from ledger_system.data.models.equipment_property import EquipmentProperty, EQUIPMENT_PROPERTY_KEYS
 from ledger_system.data.models.rule_learning import RuleLearning
 from ledger_system.data.models.document_log import DocumentLog
-from ledger_system.data.models.category import Category
 from ledger_system.data.models.material_code import MaterialCode
+from ledger_system.data.models.category import Category
 
 __all__ = [
-    "Base", "Ledger", "Inbound", "Outbound",
-    "RuleLearning", "DocumentLog", "Category", "MaterialCode"
+    "Base",
+    "Ledger",
+    "Inbound",
+    "Outbound",
+    "MaterialProperty",
+    "MATERIAL_PROPERTY_KEYS",
+    "EquipmentProperty",
+    "EQUIPMENT_PROPERTY_KEYS",
+    "RuleLearning",
+    "DocumentLog",
+    "MaterialCode",
+    "Category",
 ]
